be817fdb526e6d7dad86bf46c05994d73f1b9451
John <35469139+jheck90@users.noreply.github.com>
2023-10-04 14:16:23 -0600

    Update backend.tf

--- tests/testdata/example/backend.tf
+++ tests/testdata/example/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket               = "edo-terraform"
+    bucket               = "test-terraform"
     workspace_key_prefix = "terraform-copy"
     key                  = "tests/testdata/example/"
     region               = "us-east-1"
