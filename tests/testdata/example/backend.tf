8cb29ae15c67c19e14cfe3f073faacf02d52dc8c
John Casillas <jcasillas@gofigg.com>
2023-10-10 11:24:08 -0600

    Refactor

--- tests/testdata/example/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "edo-terraform"
-    workspace_key_prefix = "terraform-copy"
-    key                  = "tests/testdata/example/"
-    region               = "us-east-1"
-    profile              = ""
-  }
-}
--- tests/testdata/example/main.tf
+++
@@ -1,3 +0,0 @@
-module "patching" {
-   source = "git::ssh://git@bitbucket.org/figg/terraform-aws-patching.git?ref=v1.1.0"
-}
